81c207b0b1502455a4108a82af0653c2a8b7d591
suren <zxjlwt@126.com>
2018-10-16 20:04:54 +0800

    Add api for check crontab syntax

--- src/main/java/io/alauda/jenkins/devops/sync/action/KubernetesClientAction.java
+++ src/main/java/io/alauda/jenkins/devops/sync/action/KubernetesClientAction.java
@@ -1,26 +1,39 @@
 package io.alauda.jenkins.devops.sync.action;
 
+import antlr.ANTLRException;
 import hudson.Extension;
 import hudson.model.UnprotectedRootAction;
+import hudson.scheduler.CronTab;
+import hudson.scheduler.CronTabList;
+import hudson.scheduler.Hash;
+import hudson.scheduler.Messages;
 import hudson.util.HttpResponses;
 import io.alauda.devops.client.AlaudaDevOpsConfigBuilder;
 import io.alauda.devops.client.DefaultAlaudaDevOpsClient;
 import io.alauda.jenkins.devops.sync.util.CredentialsUtils;
+import io.alauda.jenkins.devops.sync.util.CronUtils;
 import io.alauda.kubernetes.client.Config;
 import io.alauda.kubernetes.client.KubernetesClientException;
+import org.apache.tools.ant.taskdefs.condition.Http;
 import org.jenkinsci.Symbol;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.ExportedBean;
 
 import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Vector;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Extension
@@ -80,6 +93,36 @@ public class KubernetesClientAction implements UnprotectedRootAction {
         }
 
         return HttpResponses.errorJSON("no debug file");
+    }
+
+    /**
+     * Do check cronTab text
+     * @param cronText cron text
+     * @return syntax check result, previous and next time
+     */
+    public HttpResponse doCronTabCheck(@QueryParameter String cronText) {
+        Map<String, String> result = new HashMap<>();
+
+        try {
+            CronUtils cron = CronUtils.create(cronText, null);
+
+            Calendar next = cron.next();
+            Calendar previous = cron.previous();
+            if(next != null) {
+                result.put("next", String.valueOf(next.getTimeInMillis()));
+            }
+
+            if(previous != null) {
+                result.put("previous", String.valueOf(previous.getTimeInMillis()));
+            }
+
+            result.put("sanity", cron.checkSanity());
+        } catch (ANTLRException e) {
+            logger.warning(String.format("cron text syntax check error: %s.", e.getMessage()));
+            result.put("error", e.getMessage());
+        }
+
+        return HttpResponses.okJSON(result);
     }
 
     public URL connectTest(String server, String credentialId) {
---
+++ src/main/java/io/alauda/jenkins/devops/sync/util/CronUtils.java
@@ -0,0 +1,89 @@
+package io.alauda.jenkins.devops.sync.util;
+
+import antlr.ANTLRException;
+import hudson.scheduler.CronTab;
+import hudson.scheduler.CronTabList;
+import hudson.scheduler.Hash;
+import hudson.scheduler.Messages;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Vector;
+
+public class CronUtils {
+    private final Vector<CronTab> tabs;
+
+    public CronUtils(Collection<CronTab> tabs) {
+        this.tabs = new Vector<>(tabs);
+    }
+
+    public static CronUtils create(@Nonnull String format, Hash hash) throws ANTLRException {
+        Vector<CronTab> r = new Vector<>();
+        int lineNumber = 0;
+        String timezone = null;
+
+        for (String line : format.split("\\r?\\n")) {
+            lineNumber++;
+            line = line.trim();
+
+            if(lineNumber == 1 && line.startsWith("TZ=")) {
+                timezone = CronTabList.getValidTimezone(line.replace("TZ=",""));
+                if(timezone == null) {
+                    throw new ANTLRException("Invalid or unsupported timezone '" + timezone + "'");
+                }
+                continue;
+            }
+
+            if(line.length()==0 || line.startsWith("#"))
+                continue;   // ignorable line
+            try {
+                r.add(new CronTab(line,lineNumber,hash,timezone));
+            } catch (ANTLRException e) {
+                throw new ANTLRException(Messages.CronTabList_InvalidInput(line,e.toString()),e);
+            }
+        }
+
+        return new CronUtils(r);
+    }
+
+    public @CheckForNull Calendar previous() {
+        Calendar nearest = null;
+        for (CronTab tab : tabs) {
+            Calendar scheduled = tab.floor(tab.getTimeZone() == null ? Calendar.getInstance() : Calendar.getInstance(tab.getTimeZone()));
+            if (nearest == null || nearest.before(scheduled)) {
+                nearest = scheduled;
+            }
+        }
+        return nearest;
+    }
+
+    public @CheckForNull Calendar next() {
+        Calendar nearest = null;
+        for (CronTab tab : tabs) {
+            Calendar scheduled = tab.ceil(tab.getTimeZone() == null ? Calendar.getInstance() : Calendar.getInstance(tab.getTimeZone()));
+            if (nearest == null || nearest.after(scheduled)) {
+                nearest = scheduled;
+            }
+        }
+        return nearest;
+    }
+
+    /**
+     * Checks if this crontab entry looks reasonable,
+     * and if not, return an warning message.
+     *
+     * <p>
+     * The point of this method is to catch syntactically correct
+     * but semantically suspicious combinations, like
+     * "* 0 * * *"
+     */
+    public String checkSanity() {
+        for (CronTab tab : tabs) {
+            String s = tab.checkSanity();
+            if(s!=null)     return s;
+        }
+        return null;
+    }
+}
