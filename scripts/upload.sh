c55b477bd0f6dd8e7637a16a180f1721becc27ff
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-03-18 17:00:23 +0800

    Handle the 404 use case

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,8 +7,14 @@ if [ ! -f "$(dirname "${BASH_SOURCE[0]}")/env.sh" ]; then
 fi
 
 source $(dirname "${BASH_SOURCE[0]}")/env.sh
-export issuer=$(curl -u admin:$JENKINS_TOKEN $JENKINS_URL/crumbIssuer/api/json -s)
-issuer=$(python -c "import json;import os;issuer=os.getenv('issuer');issuer=json.loads(issuer);print issuer['crumb']")
+
+issuer=$(curl -u admin:$JENKINS_TOKEN $JENKINS_URL/crumbIssuer/api/json -s -o /dev/null -w %{http_code})
+if [ "$issuer" == "200" ]; then
+    export issuer=$(curl -u admin:$JENKINS_TOKEN $JENKINS_URL/crumbIssuer/api/json -s)
+    issuer=$(python -c "import json;import os;issuer=os.getenv('issuer');issuer=json.loads(issuer);print issuer['crumb']")
+else
+    issuer=""
+fi
 
 curl -u admin:$JENKINS_TOKEN $JENKINS_URL/pluginManager/uploadPlugin -F "name=@$(dirname "${BASH_SOURCE[0]}")/../target/alauda-devops-sync.hpi" --header "Jenkins-Crumb: $issuer"
 curl -u admin:$JENKINS_TOKEN $JENKINS_URL/restart -X POST --header "Jenkins-Crumb: $issuer"
