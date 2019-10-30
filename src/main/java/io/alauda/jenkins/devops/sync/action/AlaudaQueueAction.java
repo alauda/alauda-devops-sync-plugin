27bf74b4195ff06b6dc5e6a1e6cdbdd273e9a575
cizezsy <zsy19980307@gmail.com>
2019-10-30 16:45:27 +0800

    [DEVOPS-2716] Hotfix: add pipeline to queue multiple times

--- src/main/java/io/alauda/jenkins/devops/sync/action/AlaudaQueueAction.java
+++ src/main/java/io/alauda/jenkins/devops/sync/action/AlaudaQueueAction.java
@@ -1,20 +1,37 @@
 package io.alauda.jenkins.devops.sync.action;
 
-import hudson.Extension;
 import hudson.model.Action;
 import hudson.model.Queue;
+import io.alauda.jenkins.devops.sync.util.NamespaceName;
 import org.kohsuke.stapler.export.ExportedBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.CheckForNull;
 import java.util.List;
 
-/**
- * Always should schedule the job which triggered by alauda devops platfrom
- */
 @ExportedBean
 public class AlaudaQueueAction implements Queue.QueueAction {
+    private static final Logger logger = LoggerFactory.getLogger(AlaudaQueueAction.class);
+
+    private NamespaceName mappedPipelineNamespaceName;
+
+    public AlaudaQueueAction(String pipelineNamespace, String pipelineName) {
+        this.mappedPipelineNamespaceName = new NamespaceName(pipelineNamespace, pipelineName);
+    }
+
     @Override
-    public boolean shouldSchedule(List<Action> list) {
+    public boolean shouldSchedule(List<Action> actionsOfItemWillBeQueued) {
+        for (Action action : actionsOfItemWillBeQueued) {
+            if (action instanceof AlaudaQueueAction) {
+                // if there is a same pipeline queued, we should not queue it again
+                if (((AlaudaQueueAction) action).getMappedPipelineNamespaceName().equals(this.mappedPipelineNamespaceName)) {
+                    logger.info("Pipeline '{}/{}' has already been added to build queue, won't schedule a new build.",
+                            mappedPipelineNamespaceName.getNamespace(), mappedPipelineNamespaceName.getName());
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
@@ -34,5 +51,9 @@ public class AlaudaQueueAction implements Queue.QueueAction {
     @Override
     public String getUrlName() {
         return "alaudaAction";
+    }
+
+    public NamespaceName getMappedPipelineNamespaceName() {
+        return mappedPipelineNamespaceName;
     }
 }
