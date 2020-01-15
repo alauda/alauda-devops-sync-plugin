2e77d05ba5c09d1968a643569d2668bd49811811
conan17 <liyinancoder@gmail.com>
2020-01-15 12:35:53 +0800

    feat(pipeline): support pipeline info dsl
    DEVOPS-2978

--- src/main/java/io/alauda/jenkins/devops/sync/listener/PipelineSyncRunListener.java
+++ src/main/java/io/alauda/jenkins/devops/sync/listener/PipelineSyncRunListener.java
@@ -33,17 +33,14 @@ import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.model.listeners.RunListener;
 import hudson.triggers.SafeTimerTask;
-import io.alauda.devops.java.client.models.V1alpha1Pipeline;
-import io.alauda.devops.java.client.models.V1alpha1PipelineConfig;
-import io.alauda.devops.java.client.models.V1alpha1PipelineStatus;
-import io.alauda.devops.java.client.models.V1alpha1PipelineStatusJenkins;
-import io.alauda.devops.java.client.models.V1alpha1PipelineStatusJenkinsBuilder;
+import io.alauda.devops.java.client.models.*;
 import io.alauda.devops.java.client.utils.DeepCopyUtils;
 import io.alauda.jenkins.devops.sync.AlaudaJobProperty;
 import io.alauda.jenkins.devops.sync.AlaudaSyncGlobalConfiguration;
 import io.alauda.jenkins.devops.sync.JenkinsPipelineCause;
 import io.alauda.jenkins.devops.sync.MultiBranchProperty;
 import io.alauda.jenkins.devops.sync.PipelineConfigToJobMapper;
+import io.alauda.jenkins.devops.sync.action.PipelineAction;
 import io.alauda.jenkins.devops.sync.client.Clients;
 import io.alauda.jenkins.devops.sync.constants.Constants;
 import io.alauda.jenkins.devops.sync.util.JenkinsUtils;
@@ -619,6 +616,7 @@ public class PipelineSyncRunListener extends RunListener<Run> {
     newPipeline.getMetadata().setAnnotations(annotations);
 
     badgeHandle(run, annotations);
+    mountActionsPipeline(run.getAllActions(), newPipeline);
 
     V1alpha1PipelineStatus status =
         createPipelineStatus(
@@ -643,6 +641,39 @@ public class PipelineSyncRunListener extends RunListener<Run> {
     cause.setNumStages(newNumStages);
     cause.setLastUpdateToAlaudaDevOps(TimeUnit.NANOSECONDS.toMillis(System.nanoTime()));
     return new Result(false);
+  }
+
+  /** Used to hang action data in the pipeline and provide it to DSL for real-time acquisition. */
+  public static void mountActionsPipeline(
+      List<? extends Action> actions, V1alpha1Pipeline pipeline) {
+    if (actions == null || pipeline == null) {
+      return;
+    }
+    List<V1alpha1PipelineStatusInfoItem> items = new ArrayList<>();
+    actions
+        .stream()
+        .filter(
+            action -> {
+              return action.getClass().equals(PipelineAction.class);
+            })
+        .forEach(
+            action -> {
+              PipelineAction pa = (PipelineAction) action;
+              pa.getItems()
+                  .forEach(
+                      item -> {
+                        V1alpha1PipelineStatusInfoItem data = new V1alpha1PipelineStatusInfoItem();
+                        data.setValue(item.getValue());
+                        data.setType(item.getType());
+                        data.setDescription(item.getDesc());
+                        data.setName(item.getName());
+                        items.add(data);
+                      });
+            });
+    if (pipeline.getStatus().getInformation() == null) {
+      pipeline.getStatus().setInformation(new V1alpha1PipelineStatusInfo());
+    }
+    pipeline.getStatus().getInformation().setItems(items);
   }
 
   private void badgeHandle(@NotNull Run run, Map<String, String> annotations) {
---
+++ src/main/java/io/alauda/jenkins/devops/sync/var/PipelineContext.java
@@ -0,0 +1,97 @@
+package io.alauda.jenkins.devops.sync.var;
+
+import static io.alauda.jenkins.devops.sync.listener.PipelineSyncRunListener.mountActionsPipeline;
+
+import com.cloudbees.groovy.cps.NonCPS;
+import com.google.gson.GsonBuilder;
+import io.alauda.devops.java.client.models.V1alpha1Pipeline;
+import io.alauda.jenkins.devops.sync.JenkinsPipelineCause;
+import io.alauda.jenkins.devops.sync.action.PipelineAction;
+import io.alauda.jenkins.devops.sync.client.Clients;
+import io.alauda.jenkins.devops.sync.util.PipelineUtils;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import net.sf.json.JSONObject;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.Whitelisted;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+
+public class PipelineContext {
+  private WorkflowRun run;
+
+  // pipeline namespace/name
+  private String namespace;
+  private String name;
+
+  public PipelineContext(WorkflowJob job) {
+    this.run = job.getLastBuild();
+    JenkinsPipelineCause cause = PipelineUtils.findAlaudaCause(run);
+    this.namespace = cause.getNamespace();
+    this.name = cause.getName();
+  }
+
+  @Whitelisted
+  public void appendInfo(String name, Object value, String type, String desc) {
+    PipelineAction action = run.getAction(PipelineAction.class);
+    if (action == null) {
+      action = new PipelineAction();
+    }
+
+    List<PipelineAction.Information> info = action.getItems();
+    info.add(new PipelineAction.Information(name, value, type, desc));
+    run.addOrReplaceAction(action);
+  }
+
+  @Whitelisted
+  public void appendInfo(String name, Object value, String type) {
+    this.appendInfo(name, value, type, "");
+  }
+
+  @Whitelisted
+  public void appendInfo(String name, Object value) {
+    this.appendInfo(name, value, value.getClass().getSimpleName().toLowerCase());
+  }
+
+  /**
+   * Update or create the same data When the name and type are the same, the same data is considered
+   */
+  @Whitelisted
+  public void createOrUpdateInfo(String name, Object value, String type, String desc) {
+    deleteInfo(name, type);
+    appendInfo(name, value, type, desc);
+  }
+
+  @Whitelisted
+  public void createOrUpdateInfo(String name, Object value, String type) {
+    this.createOrUpdateInfo(name, value, type, "");
+  }
+
+  public void deleteInfo(String name, String type) {
+    PipelineAction action = run.getAction(PipelineAction.class);
+    if (action == null) {
+      return;
+    }
+    List<PipelineAction.Information> list = action.getItems();
+    for (int index = 0; index < list.size(); index++) {
+      PipelineAction.Information item = list.get(index);
+      if (item.getName().equals(name) && item.getType().equals(type)) {
+        list.remove(item);
+      }
+    }
+    run.addOrReplaceAction(action);
+  }
+
+  @NonCPS
+  @Whitelisted
+  public Map<String, Object> getData() {
+    Map<String, Object> data = new HashMap<>();
+    V1alpha1Pipeline pipeline =
+        Clients.get(V1alpha1Pipeline.class).lister().namespace(namespace).get(name);
+    if (pipeline != null) {
+      mountActionsPipeline(run.getAllActions(), pipeline);
+      data.put("pipeline", JSONObject.fromObject(new GsonBuilder().create().toJson(pipeline)));
+    }
+    return data;
+  }
+}
